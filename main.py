60faadf5f2c1d346a5d516b69f0e655ba559e3ec
james <james@james.local>
2023-07-13 22:14:01 +0100

    Add reg

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from webscrape_cargiant import webscrape_cargiant
+from webscrape_cargiant_class import webscrape_cargiant
 
 
 template = webscrape_cargiant()
@@ -6,7 +6,13 @@ template.getCarMakes()
 
 bmw = webscrape_cargiant("bmw")
 mercedes = webscrape_cargiant("mercedes")
+lexus = webscrape_cargiant("lexus")
+voltswagen = webscrape_cargiant("Volkswagen")
 bmw.printData()
 mercedes.printData()
+lexus.printData()
+voltswagen.printData()
+
+
 bmwsearch = bmw.searchDataForCar("Model", "118D")
 mercedessearch = mercedes.searchDataForCar("Body Type", "Hatch")
