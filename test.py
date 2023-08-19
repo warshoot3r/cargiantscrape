b35b56f1ed9035557882c36212f4c4ac4dfb7afb
james <james@james.local>
2023-08-19 10:13:22 +0100

    fix tags

--- test.py
+++ test.py
@@ -8,17 +8,17 @@ DB = SQLiteDatabase()
 
 #create Telegrambot
 tb = TelegramBot(api_token=33)
-
+print("Successful teleegram bot init")
 
 data = DB.return_as_panda_dataframe()
 
 #Create selenium
 cars = WebScraperCargiant(driver="chrome", keepalive=True)
-
+print("Successful selenium init")
 # test 1
 cars.get_car_makes()
 
-
+print("Successful Car makes init")
 #test 2
 filter = {
 
@@ -26,5 +26,7 @@ filter = {
     }
 array = ["MF68CEX", "YK16ETD", "YH16JUK"]
 data_filter = DB.filter_table( filter, data, array)
+
+print("Successful filtertable")
 
 print("Successful execution")
