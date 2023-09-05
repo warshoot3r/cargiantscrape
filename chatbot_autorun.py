3a95c094ded8336baf45efd6ef32a6d2b9c4e090
james <james@james.local>
2023-09-05 14:48:35 +0100

    dont show suv , show coupe

--- chatbot_autorun.py
+++ chatbot_autorun.py
@@ -25,7 +25,7 @@ filters = {
      # No Mercedes A Classes 
      # No BMW 1 Series
      # No BMW i3's
-     'Body Type': lambda bodytype: bodytype not in ("Estate", "Coupe"), 
+     'Body Type': lambda bodytype: bodytype not in ("Estate", "SUV"), 
     'Model': lambda model: (not re.match(r"[A|1]\d+", model)) & (not model.startswith('i3')) &(not model.startswith('2 Series')) &(not model.startswith("B")) #
 }
 
