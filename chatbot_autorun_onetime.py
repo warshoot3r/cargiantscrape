07b2f57fc66e44a9f372f4770f9594e30169b09c
james <james@james.local>
2023-09-10 08:30:08 +0100

    fix csv filtering of cars

--- chatbot_autorun_onetime.py
+++ chatbot_autorun_onetime.py
@@ -113,8 +113,8 @@ if price_changed or new_cars or status_changed:
     #Send sold cars
     #Send rest of cars
     csv_dataframe = DB.filter_table(filters, database) # every car
-    available_csv = csv_dataframe.loc[csv_dataframe['CarStatus'].str.contains(r'AVAILABLE', case=True, regex=True)] # The available cars
-    not_available_csv = csv_dataframe.loc[~csv_dataframe['CarStatus'].str.contains(r'AVAILABLE', case=True, regex=True)] # The waiting cars
+    not_available_csv = csv_dataframe.loc[csv_dataframe['CarStatus'].str.contains(r'AVAILABLE', case=True, regex=True)] # The available cars
+    available_csv = csv_dataframe.loc[~csv_dataframe['CarStatus'].str.contains(r'AVAILABLE', case=True, regex=True)] # The waiting cars
     bot.send_dataframe_as_file(chat_id=chat_id, file_format="csv", dataframe=available_csv, caption="Available Cars", file_name="available")
     bot.send_dataframe_as_file(chat_id=chat_id, file_format="csv", dataframe=not_available_csv, caption="Waiting Cars", file_name="waiting")
 
