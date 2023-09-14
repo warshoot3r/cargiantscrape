599e10bceb3e6f57331e9cdd03d84f6fb112c9f8
james <james@james.local>
2023-09-14 22:25:50 +0100

    Inplemented multiple files sent in telegram

--- chatbot_autorun_onetime.py
+++ chatbot_autorun_onetime.py
@@ -114,8 +114,14 @@ if price_changed or new_cars or status_changed:
         csv_dataframe = DB.filter_table(filters, database) # every car
         not_available_csv = csv_dataframe.loc[csv_dataframe['CarStatus'].str.contains(r'AVAILABLE', case=True, regex=True)] # The available cars
         available_csv = csv_dataframe.loc[~csv_dataframe['CarStatus'].str.contains(r'AVAILABLE', case=True, regex=True)] # The waiting cars
-        bot.send_dataframe_as_file(chat_id=chat_id, file_format="csv", dataframe=available_csv, caption="Available Cars", file_name="available")
-        bot.send_dataframe_as_file(chat_id=chat_id, file_format="csv", dataframe=not_available_csv, caption="Waiting Cars", file_name="waiting")
+        # bot.send_dataframe_as_file(chat_id=chat_id, file_format="csv", dataframe=available_csv, caption="Available Cars", file_name="available")
+        # bot.send_dataframe_as_file(chat_id=chat_id, file_format="csv", dataframe=not_available_csv, caption="Waiting Cars", file_name="waiting")
+       #New way send multiple in one go.
+        data_frames = [available_csv, not_available_csv]
+        file_formats = ["csv","csv"]
+        captions = ["Available Cars", "Waiting Cars"]
+        file_names = ["available.csv", "waiting.csv"]
+        bot.send_dataframe_as_multiple_files(captions=captions,chat_id=credentials.chat_id, file_formats=file_formats, dataframes=data_frames, file_names=file_names)
 
  
     DB.close_db()
--- modules/telegram_bot.py
+++ modules/telegram_bot.py
@@ -1,6 +1,9 @@
 import requests
 import socket
 import re
+import zipfile
+import tempfile
+import os
 class TelegramBot:
     def __init__(self, api_token):
         self.api_token = api_token
@@ -128,6 +131,67 @@ class TelegramBot:
             # Remove the temporary file
             import os
             os.remove(filename)
+    def send_dataframe_as_multiple_files(self, chat_id, dataframes, file_formats=None, captions=None, file_names=None):
+        """
+                # Example usage:
+        # dataframes = [df1, df2]  # List of dataframes to send
+        # file_formats = ["csv", "xlsx"]  # List of file formats corresponding to dataframes
+        # captions = ["Caption 1", "Caption 2"]  # List of captions for files
+        # file_names = ["data1", "data2"]  # List of file names
+        # your_bot.send_dataframe_as_files(chat_id="your_chat_id_here", dataframes=dataframes, file_formats=file_formats, captions=captions, file_names=file_names)
+        """
+        if not file_formats:
+            file_formats = ["csv"] * len(dataframes)
+        if not captions:
+            captions = [""] * len(dataframes)
+        if not file_names:
+            file_names = ["data"] * len(dataframes)
+
+        if len(dataframes) != len(file_formats) or len(dataframes) != len(captions) or len(dataframes) != len(file_names):
+            raise ValueError("Length of dataframes, file_formats, captions, and file_names should be the same.")
+
+        # Create a temporary directory to store the individual files
+        temp_dir = tempfile.mkdtemp()
+
+        try:
+            file_paths = []
+            for i, dataframe in enumerate(dataframes):
+                file_format = file_formats[i]
+                file_name = f"{file_names[i]}.{file_format}"
+                file_path = os.path.join(temp_dir, file_name)
+
+                if file_format == "csv":
+                    dataframe.to_csv(file_path, index=False)
+                elif file_format == "xlsx":
+                    dataframe.to_excel(file_path, index=False)
+
+                file_paths.append(file_path)
+
+            # Create a zip file containing all the individual files
+            zip_filename = "data.zip"
+            zip_file_path = os.path.join(temp_dir, zip_filename)
+            with zipfile.ZipFile(zip_file_path, 'w') as zipf:
+                for file_path in file_paths:
+                    zipf.write(file_path, os.path.basename(file_path))
+
+            send_document_url = f"{self.base_url}sendDocument"
+            files = {"document": (zip_filename, open(zip_file_path, "rb"))}
+
+            try:
+                response = requests.post(send_document_url, data={"chat_id": chat_id}, files=files)
+                response_json = response.json()
+                if response_json["ok"]:
+                    print("Files sent successfully!", flush=True)
+                else:
+                    print("Failed to send files. Error:", response_json["description"])
+            except requests.exceptions.RequestException as e:
+                print("Error sending files:", e)
+        finally:
+            # Remove the temporary directory and its contents
+            for file_path in file_paths:
+                os.remove(file_path)
+            os.remove(zip_file_path)
+            os.rmdir(temp_dir)
     # # Create an instance of the TelegramBot class
     # bot = TelegramBot(api_token)
 
