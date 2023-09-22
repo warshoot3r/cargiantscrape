7dd9c5cea856579cdcc3fc4b6b2ec14a8bb7c1f7
james <james@james.local>
2023-09-22 23:20:47 +0100

    update time script

--- daily_report.py
+++ daily_report.py
@@ -9,26 +9,23 @@ import datetime
 import sys
 
 
-scheduled_time = datetime.time(20)# 8 PM
-print(f"Scheduled time at {scheduled_time}")
-print(f"Machine time at {datetime.datetime.now()}")
+scheduled_time_start_hour = 20
+scheduled_time_end_hour = 21
+
+print(f"Scheduled time is {scheduled_time_start_hour}:00 to {scheduled_time_end_hour}:00. Machine time -> {datetime.datetime.now().strftime('%H:%M')}")
 while True:
         current_time = datetime.datetime.now()
-        # Calculate the time difference between local time and GMT
-        time_difference = current_time - datetime.datetime.utcnow()
+        # Calculate the time difference between local time and GM
+        time_str = current_time.strftime('%H:%M')
+        if  scheduled_time_start_hour <= current_time.hour < scheduled_time_end_hour:
 
-        # Subtract the time difference to get the GMT time
-        gmt_time = current_time - time_difference
-
-        converted_current_time = gmt_time.time()
-        if converted_current_time < scheduled_time:
-            print(f"Not time yet {converted_current_time}")
-            sys.exit()
+            print(f"It's time {time_str}", flush=True)
+            break
 
         else:
-            print("Its time")
-            print(current_time)
-            break
+            print(f"Not time. Cancelling {time_str}", flush=True)
+            sys.exit()
+
 #time script
 
 
